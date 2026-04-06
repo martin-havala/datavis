1e741be5b66686dcd310a979ee7e675b7dd2e436
Martin Havala <martin-havala@smarthead.com>
2026-04-07 01:44:42 +0200

    small layout fix

--- docs/app/immutable/chunks/BrvEUWV6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_wh88ws?.base??"/datavis",a=globalThis.__sveltekit_wh88ws?.assets??s??"https://martin-havala.github.io/datavis";export{a,s as b};
---
+++ docs/app/immutable/chunks/vZ8AwIuc.js
@@ -0,0 +1,1 @@
+const a=globalThis.__sveltekit_a8g4w?.base??"/datavis",s=globalThis.__sveltekit_a8g4w?.assets??a??"https://martin-havala.github.io/datavis";export{s as a,a as b};
